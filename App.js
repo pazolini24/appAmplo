3fcd6e5ca2cfc591f8ca1437486fd62009af269f
pazolini24 <112673444+pazolini24@users.noreply.github.com>
2023-04-06 13:31:38 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Routes } from './src/routes/index.js'
+import { NavigationContainer } from '@react-navigation/native'
+
+export default function App(){
+  return(
+    <NavigationContainer>
+        <Routes/>
+    </NavigationContainer>
+  )
+}
+
+
